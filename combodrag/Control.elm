03ab37399cb0c18a6b0c55d56bc985367010db97
gmp26 <gmp26@cam.ac.uk>
2014-10-15 23:51:00 +0100

    more DD code untested

--- combodrag/Control.elm
+++ combodrag/Control.elm
@@ -3,34 +3,33 @@ module Control where
 import Model as M
 import Vector as V
 
-
 --
 -- UPDATE
 --
-activate : M.Strip -> M.GameState -> M.GameState
-activate strip gs =
-    let aStrips = map (\s -> {s | active <- True }) gs.strips
+modify: M.Strip -> (M.Strip -> M.Strip) -> M.GameState -> M.GameState
+modify strip f gs =
+    let aStrips = map (\s -> if s == strip then f s else identity) gs.strips
     in {gs | strips <- aStrips}
 
-translate : M.Strip -> M.GameState -> M.GameState
-translate strip delta gs =
-    let tStrips = map (\s -> {s | loc <- s.loc `V.plus` (M.gridDelta delta)}) gs.strips
-    in {gs | strips <- tStrips}
+startDrag : M.Strip -> M.GameState -> M.GameState
+startDrag strip gs = 
+    modify strip (\s -> {s | active <- True }) gs
 
-release : M.Strip -> M.GameState -> M.GameState
-release strip gs =
-    let rStrips = map (\s   ->  {s | active <- False
-                                ,    loc <- V.round strip.loc
-                                }) gs.strips
-    in {gs | strips <- rStrips}
+drag : M.Strip -> M.GameState -> M.GameState
+drag strip delta gs =
+    modify strip (\s -> {s | loc <- s.loc `V.plus` (M.gridDelta delta)}) gs
+
+stopDrag : M.Strip -> M.GameState -> M.GameState
+stopDrag strip gs =
+    modify strip (\s -> {s | active <- False, loc <- V.round strip.loc}) gs
 
 update : M.Event -> M.State -> M.State
 update event state = case state of
     M.GotoPlay  -> M.Play M.initialGame
     M.Play gs   -> case event of
-        Just (strip, Lift)              -> M.Play <| activate strip gs
-        Just (strip, MoveBy (dx,dy))    -> M.Play <| translate strip gs
-        Just (strip, Release)           -> M.Play <| release strip gs
+        Just (strip, Lift)              -> M.Play <| startDrag strip gs
+        Just (strip, MoveBy (dx,dy))    -> M.Play <| drag strip gs
+        Just (strip, Release)           -> M.Play <| stopDrag strip gs
         _                               -> state
 
 
